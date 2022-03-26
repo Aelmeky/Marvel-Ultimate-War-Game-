df2357b54e8e00fce2c02287eb4bf44eab0509e8
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-03-26 17:42:11 +0200

    Correcting LeaderAbilityAlreadyUsedException class spelling

---
+++ src/exceptions/LeaderAbilityAlreadyUsedException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class LeaderAbilityAlreadyUsedException extends GameActionException {
+	public LeaderAbilityAlreadyUsedException() {
+		super();
+	}
+	public LeaderAbilityAlreadyUsedException(String s) {
+		super(s);
+	}
+}
