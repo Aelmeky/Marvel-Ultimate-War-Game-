e03b479e37c188f2ec392a2d83c5cd19225014dc
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-03-26 12:52:20 +0200

    Creating GameActionException Class

---
+++ src/exceptions/GameActionException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class GameActionException extends Exception {
+	public GameActionException() {
+		super();
+	}
+	public GameActionException(String s) {
+		super(s);
+	}
+}
