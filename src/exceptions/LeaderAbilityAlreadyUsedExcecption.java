dbb991e7972d2068b76563689809eda8215e6ec8
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-03-26 13:06:11 +0200

    Creating classes for different game exceptions

---
+++ src/exceptions/LeaderAbilityAlreadyUsedExcecption.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class LeaderAbilityAlreadyUsedExcecption extends GameActionException {
+	public LeaderAbilityAlreadyUsedExcecption() {
+		super();
+	}
+	public LeaderAbilityAlreadyUsedExcecption(String s) {
+		super(s);
+	}
+}
---
+++ src/exceptions/NotEnoughResourcesException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class NotEnoughResourcesException extends GameActionException {
+	public NotEnoughResourcesException() {
+		super();
+	}
+	public NotEnoughResourcesException(String s) {
+		super(s);
+	}
+}
