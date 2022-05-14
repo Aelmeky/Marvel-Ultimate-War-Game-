8731a48415f3149d02163c339417fe5b2dfd31b0
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-05-14 16:44:18 +0200

    root effect apply method

--- src/model/effects/Root.java
+++ src/model/effects/Root.java
@@ -1,10 +1,21 @@
 package model.effects;
 
+import model.world.Champion;
+import model.world.Condition;
+
 public class Root extends Effect {
 
 	public Root( int duration) {
 		super("Root", duration, EffectType.DEBUFF);
 		
+	}
+	
+	public void apply(Champion c) {
+		c.setCondition(Condition.ROOTED);
+	}
+	
+	public void remove(Champion c) {
+		c.setCondition(Condition.ACTIVE);
 	}
 
 }
