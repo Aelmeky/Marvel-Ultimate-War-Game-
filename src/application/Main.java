10ec63a57f0639a86a994b4039126bfd9b038533
Ahmed <AhmedWael216@protonmail.com>
2022-05-29 21:00:28 +0200

    should work

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
