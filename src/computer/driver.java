ba30e7ad8dca1efaa03b0bc72f04aa17a7ba95ca
Ahmed Wael <73046395+ahmedwael216@users.noreply.github.com>
2022-06-07 09:38:49 +0200

    changes in max & Bug found in clone

--- src/computer/driver.java
+++ src/computer/driver.java
@@ -2,9 +2,12 @@ package computer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
 import model.effects.*;
 import engine.Game;
 import engine.Player;
+import exceptions.NotEnoughResourcesException;
+import exceptions.UnallowedMovementException;
 import model.abilities.Ability;
 import model.abilities.AreaOfEffect;
 import model.abilities.CrowdControlAbility;
@@ -18,6 +21,7 @@ import model.world.Hero;
 import model.world.Villain;
 
 public class driver {
+	static int i=1;
 	public static void main(String[] args) {
 		Player p1 = new Player("Ahmed");
 		Player p2 = new Player("meky");
@@ -29,24 +33,37 @@ public class driver {
 		} catch (IOException e) {
 		}
 		game.getFirstPlayer().getTeam()
-				.add(game.getAvailableChampions().get(0));
-		game.getFirstPlayer().getTeam()
 				.add(game.getAvailableChampions().get(1));
 		game.getFirstPlayer().getTeam()
-				.add(game.getAvailableChampions().get(2));
+				.add(game.getAvailableChampions().get(6));
+		game.getFirstPlayer().getTeam()
+				.add(game.getAvailableChampions().get(11));
 
 		game.getSecondPlayer().getTeam()
-				.add(game.getAvailableChampions().get(3));
-		game.getSecondPlayer().getTeam()
-				.add(game.getAvailableChampions().get(4));
+				.add(game.getAvailableChampions().get(0));
 		game.getSecondPlayer().getTeam()
 				.add(game.getAvailableChampions().get(5));
+		game.getSecondPlayer().getTeam()
+				.add(game.getAvailableChampions().get(10));
 		game.placeChampions();
 		game.prepareChampionTurns();
-
+		
+		System.out.println(game.getCurrentChampion().getName());
+		System.out.println(game.getCurrentChampion().getLocation());
+		try {
+			game.move(Direction.DOWN);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		printGame(game);
 		Game game2 = clone(game);
 		printGame(game2);
+		game2.getCurrentChampion().setCurrentActionPoints(2);
+		ArrayList<String>arr=new ArrayList<String>();
+		System.out.println(game.getCurrentChampion().getName());
+		System.out.println(game2.getCurrentChampion().getLocation());
+//		minimax(game, game2, game2.getSecondPlayer(), arr, 2);
+		
 
 	}
 
@@ -60,14 +77,15 @@ public class driver {
 		// System.out.println(game.getSecondPlayer().getTeam().get(0).getAppliedEffects().get(0));
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				if (game.getBoard()[i][j] == null) {
+				if (game.getBoard()[4-i][j] == null) {
 					System.out.print(" ");
 				}
-				if (game.getBoard()[i][j] != null) {
-					if (game.getBoard()[i][j] instanceof Champion) {
+				if (game.getBoard()[4-i][j] != null) {
+					if (game.getBoard()[4-i][j] instanceof Champion) {
 						System.out.print("C");
+
 					}
-					if (game.getBoard()[i][j] instanceof Cover) {
+					if (game.getBoard()[4-i][j] instanceof Cover) {
 						System.out.print("v");
 					}
 				}
@@ -89,29 +107,44 @@ public class driver {
 		newGame.prepareChampionTurns();
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				if (newGame.getBoard()[i][j] instanceof Cover) {
-					newGame.getBoard()[i][j] = null;
+				if (newGame.getBoard()[4-i][j] instanceof Cover) {
+					newGame.getBoard()[4-i][j] = null;
 				}
 			}
 		}
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
-				if (game.getBoard()[i][j] != null) {
-					// if(game.getBoard()[i][j] instanceof Champion){
-					// System.out.println(((Champion)game.getBoard()[i][j]).getName()+"||"+((Champion)newGame.getBoard()[i][j]).getName());
-					// }
-					if (game.getBoard()[i][j] instanceof Cover) {
-						Cover c = new Cover(i, j);
-						c.setCurrentHP(((Cover) game.getBoard()[i][j])
-								.getCurrentHP());
-						newGame.getBoard()[i][j] = c;
+				if (game.getBoard()[4-i][j] != null) {
+					if(game.getBoard()[i][j] instanceof Champion){
+						//System.out.print(((Champion)game.getBoard()[i][j]).getName());
+						Champion c=getChampionByName(newGame,((Champion)game.getBoard()[i][j]).getName());
+						//System.out.println(c.getName());
+						newGame.getBoard()[4-i][j]=c;
 					}
-				} else {
-					continue;
-				}
+					if (game.getBoard()[4-i][j] instanceof Cover) {
+						Cover c = new Cover(i, j);
+						c.setCurrentHP(((Cover) game.getBoard()[4-i][j])
+								.getCurrentHP());
+						newGame.getBoard()[4-i][j] = c;
+					}
+				}				
 			}
 		}
 		return newGame;
+	}
+
+	public static Champion getChampionByName(Game game, String name) {
+		for(Champion c:game.getFirstPlayer().getTeam()){
+			if(c.getName().equals(name)){
+				return c;
+			}
+		}
+		for(Champion c:game.getSecondPlayer().getTeam()){
+			if(c.getName().equals(name)){
+				return c;
+			}
+		}
+		return null;
 	}
 
 	public static void cloneChampions(Player p1, Player p2) {
@@ -178,12 +211,12 @@ public class driver {
 	}
 
 	public static int evaluate(Game ogame, Game ngame, Player me) {
-		Object[][] n=ngame.getBoard();
-		Object[][] o=ogame.getBoard();
-		if(ngame.getSecondPlayer().getTeam().size()==0){
+		Object[][] n = ngame.getBoard();
+		Object[][] o = ogame.getBoard();
+		if (ngame.getSecondPlayer().getTeam().size() == 0) {
 			return Integer.MIN_VALUE;
 		}
-		if(ngame.getFirstPlayer().getTeam().size()==0){
+		if (ngame.getFirstPlayer().getTeam().size() == 0) {
 			return Integer.MAX_VALUE;
 		}
 		int sum = 0;
@@ -191,25 +224,27 @@ public class driver {
 			for (int j = 0; j < 5; j++) {
 				if (o[i][j] == n[i][j]) {
 					continue;
-				} else if(o[i][j] instanceof Champion) {
+				} else if (o[i][j] instanceof Champion) {
 					if (isFriend(me, ((Champion) n[i][j]))) {
-						//effect on a friend
-						if(n[i][j]==null){
-							//a friend got killed
-							sum-=2*((Champion) o[i][j]).getMaxHP();
-						}else{
-							//A friend got damaged
-							sum-=((Champion) o[i][j]).getCurrentHP()-((Champion) n[i][j]).getCurrentHP();
+						// effect on a friend
+						if (n[i][j] == null) {
+							// a friend got killed
+							sum -= 2 * ((Champion) o[i][j]).getMaxHP();
+						} else {
+							// A friend got damaged
+							sum -= ((Champion) o[i][j]).getCurrentHP()
+									- ((Champion) n[i][j]).getCurrentHP();
 						}
 					}
 					if (!isFriend(me, ((Champion) n[i][j]))) {
-						//effect on an enemy
-						if(n[i][j]==null){
-							//killed an enemy
-							sum+=2*((Champion) o[i][j]).getMaxHP();
-						}else{
-							//damaged an enemy
-							sum+=((Champion) o[i][j]).getCurrentHP()-((Champion) n[i][j]).getCurrentHP();
+						// effect on an enemy
+						if (n[i][j] == null) {
+							// killed an enemy
+							sum += 2 * ((Champion) o[i][j]).getMaxHP();
+						} else {
+							// damaged an enemy
+							sum += ((Champion) o[i][j]).getCurrentHP()
+									- ((Champion) n[i][j]).getCurrentHP();
 						}
 					}
 				}
@@ -227,104 +262,184 @@ public class driver {
 		return false;
 	}
 
-	public static ArrayList<String> max(Game oldGame,Game game,Champion c,ArrayList<String> arr,Player p ){
-		if(c.getCurrentActionPoints()==0) {
-			int x=evaluate(oldGame,game,p);
+ public static ArrayList<String> max(Game oldGame, Game game, Champion c){
+	// ArrayList<String> arr, Player p) {
+	// if (c.getCurrentActionPoints() == 0) {
+	// int x = evaluate(oldGame, game, p);
+	// arr.add(x + "");
+	// return arr;
+	// }
+	// System.out.println("here");
+	// try {
+	// Game ngame = clone(game);
+	// ngame.move(Direction.UP);
+	// arr.add("moveup");
+	// arr = max(oldGame, ngame, c, arr, p);
+	// return arr;
+	// } catch (Exception e) {
+	// }
+	// try {
+	// Game ngame = clone(game);
+	// game.move(Direction.DOWN);
+	// arr.add("moveup");
+	// arr = max(oldGame, ngame, c, arr, p);
+	// return arr;
+	// } catch (Exception e) {
+	// }
+	// try {
+	// Game ngame = clone(game);
+	// game.move(Direction.RIGHT);
+	// arr.add("moveup");
+	// arr = max(oldGame, ngame, c, arr, p);
+	// return arr;
+	// } catch (Exception e) {
+	// }
+	// try {
+	// Game ngame = clone(game);
+	// game.move(Direction.LEFT);
+	// arr.add("moveup");
+	// arr = max(oldGame, ngame, c, arr, p);
+	// return arr;
+	// } catch (Exception e) {
+	// }
+	// try {
+	// game.useLeaderAbility();;
+	// sol.add("leaderability");
+	// } catch (Exception e) {}
+	//
+	// try {
+	// game.attack(Direction.UP);
+	// sol.add("attackup");
+	// } catch (Exception e) {}
+	// try {
+	// game.attack(Direction.DOWN);
+	// sol.add("attackdown");
+	// } catch (Exception e) {}
+	// try {
+	// game.attack(Direction.RIGHT);
+	// sol.add("attackright");
+	// } catch (Exception e) {}
+	// try {
+	// game.attack(Direction.LEFT);
+	// sol.add("attackleft");
+	// } catch (Exception e) {}
+	// for(int i=0;i<c.getAbilities().size();i++){
+	// Ability a=c.getAbilities().get(i);
+	// if (a.getCastArea() == AreaOfEffect.DIRECTIONAL) {
+	// try {
+	// game.castAbility(a);;
+	// sol.add("cast"+i);
+	// } catch (Exception e) {}
+	// }
+	// if (a.getCastArea() == AreaOfEffect.SINGLETARGET) {
+	// //xyAbilities.add(a);
+	// } else {
+	// try {
+	// game.castAbility(a);;
+	// sol.add("cast"+i);
+	// } catch (Exception e) {}
+	// }
+	// }
+return null;
+}
+
+	public static ArrayList<String> getAvailableActions() {
+		ArrayList<String> arr = new ArrayList<String>();
+		arr.add("moveup");
+		arr.add("moveleft");
+		arr.add("moveright");
+		arr.add("movedown");
+//		arr.add("attackup");
+//		arr.add("attackleft");
+//		arr.add("attackwright");
+//		arr.add("attackdown");
+//		arr.add("useleaderability");
+//		arr.add("ability0");
+//		arr.add("ability1");
+//		arr.add("ability2");
+		return arr;
+	}
+	public static ArrayList<String> minimax(Game oldgame,Game game,Player p,ArrayList<String>arr, int depth){
+		ArrayList<String>sol=null;
+		System.out.println(i+" | "+depth+" | "+arr);
+		i++;
+		if(depth==0){
+			int x=evaluate(oldgame, game, p);
 			arr.add(x+"");
 			return arr;
 		}
-		System.out.println("here");
-		try {
-			Game ngame=clone(game);
-			ngame.move(Direction.UP);
-			arr.add("moveup");
-			arr=max(oldGame,ngame,c,arr,p);
-			return arr;
-		} catch (Exception e) {}
-		try {
-			Game ngame=clone(game);
-			game.move(Direction.DOWN);
-			arr.add("moveup");
-			arr=max(oldGame,ngame,c,arr,p);
-			return arr;
-		} catch (Exception e) {}
-		try {
-			Game ngame=clone(game);
-			game.move(Direction.RIGHT);
-			arr.add("moveup");
-			arr=max(oldGame,ngame,c,arr,p);
-			return arr;
-		} catch (Exception e) {}
-		try {
-			Game ngame=clone(game);
-			game.move(Direction.LEFT);
-			arr.add("moveup");
-			arr=max(oldGame,ngame,c,arr,p);
-			return arr;
-		} catch (Exception e) {}
+		if(isFriend(p, game.getCurrentChampion())){
+			int value=Integer.MIN_VALUE;
+			for(String s:getAvailableActions()){
+				if(depth==1){
+					System.out.println("here");
+				}
+				switch (s) {
+				case "moveup":
+					try {
+						Game ngame=clone(game);
+						ngame.move(Direction.UP);
+						ArrayList<String>arr2=(ArrayList<String>) arr.clone();
+						arr2.add("moveup");
+						ArrayList<String> arr3 =minimax(oldgame, ngame, p, arr2, depth-1);
+						int x=Integer.parseInt(arr3.get(arr3.size()-1));
+						if(x<value){
+							value=x;
+							sol=arr3;
+						}	
+					} catch (Exception e) {}
+					break;
+				case "movedown":
+					try {
+						Game ngame=clone(game);
+						ngame.move(Direction.DOWN);
+						ArrayList<String>arr2=(ArrayList<String>) arr.clone();
+						arr2.add("movedown");
+						ArrayList<String> arr3 =minimax(oldgame, ngame, p, arr2, depth-1);
+						int x=Integer.parseInt(arr3.get(arr3.size()-1));
+						if(x<value){
+							value=x;
+							sol=arr3;
+						}	
+					} catch (Exception e) {}
+					break;
+				case "moveleft":
+					try {
+						Game ngame=clone(game);
+						ngame.move(Direction.LEFT);
+						ArrayList<String>arr2=(ArrayList<String>) arr.clone();
+						arr2.add("moveleft");
+						ArrayList<String> arr3 =minimax(oldgame, ngame, p, arr2, depth-1);
+						int x=Integer.parseInt(arr3.get(arr3.size()-1));
+						if(x<value){
+							value=x;
+							sol=arr3;
+						}	
+					} catch (Exception e) {}
+					break;
+				case "moveright":
+					try {
+						Game ngame=clone(game);
+						ngame.move(Direction.RIGHT);
+						ArrayList<String>arr2=(ArrayList<String>) arr.clone();
+						arr2.add("movereight");
+						ArrayList<String> arr3 =minimax(oldgame, ngame, p, arr2, depth-1);
+						int x=Integer.parseInt(arr3.get(arr3.size()-1));
+						if(x<value){
+							value=x;
+							sol=arr3;
+						}	
+					} catch (Exception e) {}
+					break;
+				}
+			}
+			
+			return sol;
+		}else{
+			return null;
+		}
 		
-		
-		
-		
-		
-//		try {
-//			game.useLeaderAbility();;
-//			sol.add("leaderability");
-//		} catch (Exception e) {}
-//		
-//		try {
-//			game.attack(Direction.UP);
-//			sol.add("attackup");
-//		} catch (Exception e) {}
-//		try {
-//			game.attack(Direction.DOWN);
-//			sol.add("attackdown");
-//		} catch (Exception e) {}
-//		try {
-//			game.attack(Direction.RIGHT);
-//			sol.add("attackright");
-//		} catch (Exception e) {}
-//		try {
-//			game.attack(Direction.LEFT);
-//			sol.add("attackleft");
-//		} catch (Exception e) {}
-//		for(int i=0;i<c.getAbilities().size();i++){
-//			Ability a=c.getAbilities().get(i);
-//			if (a.getCastArea() == AreaOfEffect.DIRECTIONAL) {
-//				try {
-//					game.castAbility(a);;
-//					sol.add("cast"+i);
-//				} catch (Exception e) {}
-//			}
-//			if (a.getCastArea() == AreaOfEffect.SINGLETARGET) {
-//				//xyAbilities.add(a);
-//			} else {
-//				try {
-//					game.castAbility(a);;
-//					sol.add("cast"+i);
-//				} catch (Exception e) {}
-//			}
-//		}
-		return null;
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
